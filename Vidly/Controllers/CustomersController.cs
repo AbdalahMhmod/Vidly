142ed793900ac0f3efea70b7805ecdc8953ea401
AbdallahM7jkklajhhhj <abdullahmahmoud4550@gmail.com>
2021-03-19 17:47:38 +0200

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customer> _customers;
+
+        public CustomersController(List<Customer> customers)
+        {
+            _customers = customers;
+
+        }
+       
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id = 1,Name = "aya"},
+                new Customer {Id = 2,Name = "rania"},
+                new Customer {Id = 3,Name = "mohamed"},
+                new Customer {Id = 4,Name = "youssef"}
+            };
+
+            var viewmodel = new RandomMoviewViewModel
+            {
+                Customers = customers
+            };
+            return View(viewmodel);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            return Content("hi"+id);
+        }
+    }
+}
